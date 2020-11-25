f2ec9d99127df4e28d4845ee5f50d72e0dd647bd
Alvy <floressuarezalvaro@gmail.com>
2020-11-24 19:16:03 -0800

    cleaned up and added .then

--- index.js
+++ index.js
@@ -4,58 +4,56 @@ const util = require('util')
 
 const writeGeneratorFile = util.promisify(fs.writeFile);
 
-// array of questions for user
-const questions = [
-    inquirer.prompt(
-        [   
-            {
-                type: 'input',
-                name: 'Username',
-                message: 'What is your username?',
-            },
-            {
-                type: 'input',
-                name: 'Title',
-                message: 'What is your project title?',
-            },
-            {
-                type: 'input',
-                name: 'Description',
-                message: 'Provide me with a description of your project.',
-            },
-            {
-                type: 'input',
-                name: 'Installation',
-                message: 'How do I install your project?',
-            },
-            {
-                type: 'input',
-                name: 'Usage',
-                message: 'Please provide me with usage information.',
-            },
-            {
-                type: 'input',
-                name: 'Contributions',
-                message: 'Who contributed to this project?',
-            },
-            {
-                type: 'input',
-                name: 'Test',
-                message: 'How do I test your project? ',
-            },
-            {
-                type: 'checkbox',
-                message: 'What license would you like to use?',
-                name: 'License',
-                choices: ['MIT license', 'Apache License 2.0', 'GNU General Public License v3.0', 'BSD 2-Clause "Simplified" License'],
-            }
-        ]
-    )
-];
+// questions for user
+inquirer
+    .prompt([   
+    {
+        type: 'input',
+        name: 'Username',
+        message: 'What is your username?',
+    },
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide me with a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'How do I install your project?',
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Please provide me with usage information.',
+    },
+    {
+        type: 'input',
+        name: 'Contributions',
+        message: 'Who contributed to this project?',
+    },
+    {
+        type: 'input',
+        name: 'Test',
+        message: 'How do I test your project? ',
+    },
+    {
+        type: 'checkbox',
+        name: 'License',
+        message: 'What license would you like to use?',
+        choices: ['MIT license', 'Apache License 2.0', 'GNU General Public License v3.0', 'BSD 2-Clause "Simplified" License'],
+        type: 'input',
+    }
+])
+// .then(({Username, Title, Description, Installation, Usage, Contributions, Test, License}) => {
+//     return writeToFile("README.md", html);
+// })
 
-// function to write README file
-// function writeToFile(fileName, data) {
-// }
 
 // function to initialize program
 // function init() {
